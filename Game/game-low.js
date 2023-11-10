dcea1a44a56ef68709d238d609a6b9c402120165
Your Name~ <harish.s@google.com>
2023-11-10 17:42:26 +0530

    v1.2

--- Game/game-low.js
+++ Game/game-low.js
@@ -1,5 +1,20 @@
 var audio = new Audio("./bgm/Easy.mp3");
-audio.play();
+let volume=document.getElementById("volume");
+let mute=document.getElementById("mute");
+let i=0;
+sound.onclick=()=>{
+  if(i==0){
+  audio.play();
+  volume.style.display="block";
+  mute.style.display="none";
+  i=1;}
+ else{
+     audio.pause();
+     volume.style.display="none";
+     mute.style.display="block";
+     i=0;
+   }
+}
 let india=document.getElementById('india');
 let australia=document.getElementById("australia");
 let brazil=document.getElementById("brazil");
